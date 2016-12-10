f0c77754c5f6ea60e2b4032e733ce334a6d9ba8e
Sir Garbagetruck <truck@oobidoob.home.amigadns.net>
2016-12-10 11:54:45 +0200

    split into workspace for code ease

---
+++ workspace/basecode.lua
@@ -0,0 +1,36 @@
+-- pico-8 game shell
+-- truck
+
+function _init()
+	--
+end
+
+function _draw()
+	statusbar()
+	--
+end
+
+function _update()
+	cls()
+end
+
+function getjdir()
+ d = 0
+ if (btn(0)) then
+  d = 4
+ end
+ if (btn(1)) then
+  d = 3
+ end
+ if (btn(2)) then
+  d = 1
+ end
+ if (btn(3)) then
+  d = 2
+ end
+ return d
+end
+
+function statusbar()
+	print("L3 H ***** W:--> ",0,8*16)
+end
