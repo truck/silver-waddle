cbec3fda7967ec2fc6272ee0601f75ce6b312313
Sir Garbagetruck <truck@oobidoob.home.amigadns.net>
2016-12-10 21:18:11 +0200

    main state update stuffs and error case

--- workspace/basecode.lua
+++ workspace/basecode.lua
@@ -1,7 +1,7 @@
 -- pico-8 game shell
 -- truck
 
-pi = 3.14159265
+cstate = 'intro'
 
 function _init()
 -- only init srand if we want to have the same results
@@ -22,6 +22,20 @@ function _draw()
 end
 
 function _update()
+  uf = {
+  ['intro'] = function() do_intro() end,
+  ['help'] = function() do_help() end,
+  ['menu'] = function() do_menu() end,
+  ['move'] = function() do_move() end,
+  ['dlog'] = function() do_dialog() end,
+  ['win'] = function() do_win() end,
+  ['die'] = function() do_die() end}
+
+	if uf[cstate] then
+		uf[cstate]()
+	else
+		do_bug()
+	end
 
 end
 
@@ -45,4 +59,29 @@ end
 function statusbar()
 -- lame. fix.
 	print("L3 H ***** W:--> ",0,8*16)
+end
+
+function do_intro(  )
+end
+
+function do_help(  )
+end
+
+function do_menu(  )
+end
+
+function do_move(  )
+end
+
+function do_dialog(  )
+end
+
+function do_win(  )
+end
+
+function do_die(  )
+end
+
+function do_bug(  )
+	assert(0==1)
 end
