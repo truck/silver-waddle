2fe4fbfcd21f478d5e3ba382a70604a27488b43f
Sir Garbagetruck <truck@oobidoob.home.amigadns.net>
2016-12-10 19:26:32 +0200

    Faces. lots. bodies next.

--- workspace/faces.lua
+++ workspace/faces.lua
@@ -2,11 +2,27 @@
 
 function facebase( x,ft )
 	x=x or 0
-	circfill(x+20,100,20,14)
-	circfill(x+20,130,15,14)
 	face = unpackface(ft)
+	cl = {14,12,3}
+	cl[0]=6
+	circfill(x+20,100,20,cl[face['s']])
+	circfill(x+20,130,15,cl[face['s']])
+
 	spr(face['m']+16,x+12,104)
 	spr(face['m']+16,x+20,104,1,1,true)
+
+	spr(face['n']+20,x+12,88)
+	spr(face['n']+20,x+20,88,1,1,true)
+
+	spr(face['i']+24,x+6,78)
+	spr(face['i']+24,x+26,78,1,1,true)
+
+	spr(face['e']+28,x+2,104)
+	spr(face['e']+28,x+30,104,1,1,true)
+
+	-- spr(face['h']+16,x+12,104)
+	-- spr(face['h']+16,x+20,104,1,1,true)
+
 end
 
 -- return: mouth, nose, eyes, ears, hat, skin
@@ -21,7 +37,7 @@ function unpackface( f )
 	f=f-(i*16)
 	n = flr(f/4)
 	m = f-(n*4)
---	print("m:"..m..",n:"..n..",i:"..i..",e:"..e..",h:"..h..",s:"..s)
+	print("m:"..m..",n:"..n..",i:"..i..",e:"..e..",h:"..h..",s:"..s,0,40,7)
 	return({m=m,n=n,i=i,e=e,h=h,s=s})
 end
 
