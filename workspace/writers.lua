700a9f12e52fc7f877ee874c2c63e9fb97608fbc
Sir Garbagetruck <truck@oobidoob.home.amigadns.net>
2016-12-11 00:30:52 +0200

    border fixed (left)

--- workspace/writers.lua
+++ workspace/writers.lua
@@ -9,18 +9,18 @@ function border( x,y,w,h )
 	spr(1,x1,y1+h*8,1,1,false,true)
 	spr(1,x1+w*8,y1+h*8,1,1,true,true)
 
-	for i=x+1,w-1 do
+	for i=1,w-1 do
 		spr(2,x1+i*8,y1)
 		spr(2,x1+i*8,y1+h*8)
 	end
 
-	for i=y+1,h-1 do
+	for i=1,h-1 do
 		spr(3,x1,y1+i*8)
 		spr(3,x1+w*8,y1+i*8)
 	end
 
-	for i=x+1,w-1 do
-		for j=y+1,h-1 do
+	for i=1,w-1 do
+		for j=1,h-1 do
 			spr(9,x1+i*8,y1+j*8)
 		end
 	end
