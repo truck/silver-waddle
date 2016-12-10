94d386f0643f999be946ae0548c113f4f97fd47f
Sir Garbagetruck <truck@oobidoob.home.amigadns.net>
2016-12-11 00:40:56 +0200

    frame adjustment

--- workspace/writers.lua
+++ workspace/writers.lua
@@ -11,12 +11,12 @@ function border( x,y,w,h )
 
 	for i=1,w-1 do
 		spr(2,x1+i*8,y1)
-		spr(2,x1+i*8,y1+h*8)
+		spr(2, x1+i*8,y1+h*8, 1,1, false, true)
 	end
 
 	for i=1,h-1 do
-		spr(3,x1,y1+i*8)
-		spr(3,x1+w*8,y1+i*8)
+		spr(3, x1,y1+i*8)
+		spr(3, x1+w*8,y1+i*8, 1,1, true)
 	end
 
 	for i=1,w-1 do
