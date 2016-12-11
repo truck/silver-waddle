e5555c534a14b6e73cd3dbdfab7ea79633d99d28
Sir Garbagetruck <truck@oobidoob.home.amigadns.net>
2016-12-11 16:27:21 +0200

    calculated the amount of memory (around 18,500 "rooms")

--- workspace/localmap.lua
+++ workspace/localmap.lua
@@ -1,6 +1,6 @@
 -- local map generation from a seed
 
-function mapunpack( s )
+function roomunpack( s )
 -- 16x16 (256 cells)
 -- border
 
@@ -10,7 +10,7 @@ end
 
 function roompack()
 	r = ''
-	s = 'abcdefg'
+	s = 'abcd'
 	for i=1,16 do
 		x=flr(rnd(#s)+1)
 		c = sub(s,x,x)
@@ -18,3 +18,5 @@ function roompack()
 	end
 	return r
 end
+
+-- we can have around 18500 rooms in memory
